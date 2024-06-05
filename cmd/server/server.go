dcbed3aaa7293111c74aa56f6fba111fc0726c6a
julio.fernandes <juliocfernands@gmail.com>
2024-06-04 22:08:19 -0300

    inicio

---
+++ cmd/server/server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"graphql/graphql/graph"
+	"log"
+	"net/http"
+	"os"
+
+	"./graphql/graph/internal/database"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Default().Fatalf("failed to open database:%v", err)
+	}
+	defer db.Close()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+	categoryDB := database.NewCategory(db)
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDB,
+	}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module graphql
+
+go 1.22.1
+
+require (
+	github.com/99designs/gqlgen v0.17.47
+	github.com/google/uuid v1.6.0
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible
+	github.com/vektah/gqlparser/v2 v2.5.12
+)
